b7a4719aac02c926f026f7f5780296f5d4958e2e
RuizLenato <ruizlenato@proton.me>
2023-12-22 12:08:07 -0300

    Feat: Adds tiktok support

---
+++ handler/tiktok/api.go
@@ -0,0 +1,62 @@
+package tiktok
+
+import (
+	"context"
+	"encoding/json"
+	"gSmudgeAPI/cache"
+	"gSmudgeAPI/handler"
+	"gSmudgeAPI/utils"
+	"log"
+	"net/http"
+	"regexp"
+	"slices"
+	"time"
+
+	"github.com/tidwall/gjson"
+)
+
+func TikTokIndexer(w http.ResponseWriter, r *http.Request) {
+	resp, _ := http.Get(r.URL.Query().Get("url"))
+	VideoID := (regexp.MustCompile((`/(?:video|v)/(\d+)`))).FindStringSubmatch(resp.Request.URL.String())[1]
+	Headers := map[string]string{"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"}
+	Query := map[string]string{
+		"aweme_id": string(VideoID),
+		"aid":      "1128",
+	}
+	body := utils.GetResBody("https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/", Query, Headers)
+	caption := gjson.GetBytes(body, "aweme_list.0.desc").String()
+	indexedMedia := &handler.IndexedMedia{}
+
+	if slices.Contains([]int{2, 68, 150}, int(gjson.GetBytes(body, "aweme_list.0.aweme_type").Int())) {
+		for _, p := range gjson.GetBytes(body, "aweme_list.0.image_post_info.images").Array() {
+			indexedMedia.Medias = append(indexedMedia.Medias, handler.Medias{
+				Width:  int(p.Get("display_image.width").Int()),
+				Height: int(p.Get("display_image.height").Int()),
+				Source: p.Get("display_image.url_list.0").String(),
+				Video:  false,
+			})
+		}
+	} else {
+		for _, v := range gjson.GetBytes(body, "aweme_list.0.video.play_addr").Array() {
+			indexedMedia.Medias = append(indexedMedia.Medias, handler.Medias{
+				Width:  int(v.Get("width").Int()),
+				Height: int(v.Get("height").Int()),
+				Source: v.Get("url_list.0").String(),
+				Video:  true,
+			})
+		}
+	}
+
+	ixt := handler.IndexedMedia{
+		URL:     resp.Request.URL.String(),
+		Medias:  indexedMedia.Medias,
+		Caption: caption}
+
+	jsonResponse, _ := json.Marshal(ixt)
+	err := cache.GetRedisClient().Set(context.Background(), VideoID, jsonResponse, 24*time.Hour*60).Err()
+	if err != nil {
+		log.Println("Error setting cache:", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(ixt)
+}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"gSmudgeAPI/cache"
 	"gSmudgeAPI/handler"
 	"gSmudgeAPI/handler/instagram"
+	"gSmudgeAPI/handler/tiktok"
 	"gSmudgeAPI/handler/twitter"
 	"net/http"
 	"regexp"
@@ -19,6 +20,9 @@ func cacheMiddleware(next http.HandlerFunc) http.HandlerFunc {
 			key = (regexp.MustCompile((`.*(?:twitter|x).com/.+status/([A-Za-z0-9]+)`))).FindStringSubmatch(r.URL.Query().Get("url"))[1]
 		} else if strings.HasPrefix(r.RequestURI, "/instagram?") {
 			key = (regexp.MustCompile((`(?:reel|p)/([A-Za-z0-9_-]+)`))).FindStringSubmatch(r.URL.Query().Get("url"))[1]
+		} else if strings.HasPrefix(r.RequestURI, "/tiktok?") {
+			resp, _ := http.Get(r.URL.Query().Get("url"))
+			key = (regexp.MustCompile((`/(?:video|v)/(\d+)`))).FindStringSubmatch(resp.Request.URL.String())[1]
 		} else {
 			key = r.RequestURI
 		}
@@ -39,6 +43,7 @@ func main() {
 	http.HandleFunc("/instagram", cacheMiddleware(instagram.InstagramIndexer))
 	http.HandleFunc("/twitter", cacheMiddleware(twitter.TwitterIndexer))
 	http.HandleFunc("/x", cacheMiddleware(twitter.TwitterIndexer))
+	http.HandleFunc("/tiktok", cacheMiddleware(tiktok.TikTokIndexer))
 
 	fmt.Print("Starting gSmudgeAPI server on port 6969\n")
 	err := http.ListenAndServe(":6969", nil)
